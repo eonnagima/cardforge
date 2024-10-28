7944aa09bee7c263682eb8c70ec0b30ad3d8fa5f
ben devos <bendevos07@gmail.com>
2024-10-28 11:09:21 +0100

    feat: frontend code of header

---
+++ includes/header.inc.php
@@ -0,0 +1,15 @@
+<header>
+    <nav>
+        <a href="index.php" class="logo"></a>
+        <h2 class="app-name">cardforge</h2>
+        <label for="hb-account" class="header-avatar" style="background-image: url('./assets/img/user-avatar/placeholder.jpg');"></label>
+        <input type="checkbox" id="hb-account" class="hidden">
+    </nav>
+    <nav>
+        <a href="cart/php" class="header-cart cart-icon"></a>
+        <form class="search-bar" method="post" action="">
+            <input type="text" name="search-request" placeholder="I'm looking for...">
+            <input type="submit" value="" class="search-icon">
+        </form>
+    </nav>
+</header>
--- index.php
+++ index.php
@@ -6,8 +6,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home | Cardforge</title>
+    <link rel="stylesheet" href="./css/fonts.css">
+    <link rel="stylesheet" href="./css/style.css">
 </head>
 <body>
-    
+    <?php include_once __DIR__."/includes/header.inc.php";?>
 </body>
 </html>
