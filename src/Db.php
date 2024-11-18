bae731de14cf4dd47386ad204e217726d47b9c08
ben devos <bendevos07@gmail.com>
2024-11-18 17:37:47 +0100

    refactor: settings file > hard coded db connection

--- src/Db.php
+++ src/Db.php
@@ -2,12 +2,14 @@
 
 namespace Codinari\Cardforge;
 
+include_once __DIR__.'/../settings/config.php';
+
 class Db{
     private static $conn = null;
 
     public static function getConnection(){
         if(self::$conn === null){
-            return self::$conn = new \PDO('mysql:host=localhost;dbname=cardforge', 'root', '');
+            return self::$conn = new \PDO('mysql:host='.CONFIG['db']['host'].';dbname='.CONFIG['db']['dbname'], CONFIG['db']['user'], CONFIG['db']['password']);
         }else{
             return self::$conn;
         }
