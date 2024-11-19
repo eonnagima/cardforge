7b669835894af505fdf3bfca85e3a32806fd257d
ben devos <bendevos07@gmail.com>
2024-11-19 22:58:04 +0100

    style: admin add item page

--- admin/add-product.php
+++ admin/add-product.php
@@ -1,20 +1,72 @@
 <?php
 
-    include_once("../classes/Db.php");
-    include_once("../functions.php");
+    require_once __DIR__."/../bootstrap.php";
+    use Codinari\Cardforge\Franchise;
 
-    session_start();
-
-    // Check if the user is logged in
-    if (!isset($_SESSION['loggedIn']) || $_SESSION['loggedIn'] !== true) {
-        // Redirect the user to the login page or display an error message
-        header('Location: ../login.php');
-        exit;
+    if(empty($user) || $user->getRole() !== "admin"){
+        header("Location: ../index.php");
+        exit();
     }
 
-    if(!checkAdmin($_SESSION['user'])){
-        header('Location: ../index.php');
-        exit;
-    }
-
-    echo "Welcome to the admin page!";
+    $allFranchises = Franchise::getAll();
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Add Product | Cardforge</title>
+    <?php include_once __DIR__."/../includes/adminstylesheets.inc.php";?>
+</head>
+<body>
+    <?php include_once __DIR__."/../includes/adminheader.inc.php";?>
+    <main>
+        <h1>Add New Product</h1>
+        <?php if(isset($error)):?>
+            <div class="error"><?php echo $error;?></div>
+        <?php endif;?>
+        <form class="form" action="" method="post">
+            <section>
+                <div class="input-wrap">
+                    <label for="name">Product Name<span>*</span></label>
+                    <input type="text" id="name" name="name" required>
+                </div>
+                <div class="input-wrap">
+                    <label for="description">Product Description<span>*</span></label>
+                    <textarea id="description" name="description" required></textarea>
+                </div>
+                <div class="input-wrap">
+                    <label for="franchise">Product Franchise<span>*</span></label>
+                    <select id="franchise" name="franchise" required>
+                        <option>Select Franchise</option>
+                        <?php foreach($allFranchises as $franchise):?>
+                            <option value="<?php echo $franchise['alias'];?>"><?php echo $franchise['name'];?></option>
+                        <?php endforeach;?>
+                    </select>
+                </div>
+                <div class="input-wrap">
+                    <label for="set-name">Set Name</label>
+                    <input type="text" id="set-name" name="setName">
+                </div>
+                <!-- input release date -->
+                <div class="input-wrap">
+                    <label for="release-date">Release Date</label>
+                    <input type="date" id="release-date" name="releaseDate">
+                </div>
+                <div class="input-wrap">
+                    <label for="price">Product Price<span>*</span></label>
+                    <input type="text" id="price" name="price" required>
+                </div>
+                <div class="input-wrap">
+                    <label for="stock">Stock<span>*</span></label>
+                    <input type="text" id="stock" name="stock" required>
+                </div>         
+            </section>
+            <div class="seperator"></div>
+            <section>
+                <input class="btn" type="submit" value="LOGIN">
+                <a class="btn btn--secondary" href="signup.php">Back to Dashboard</a>
+            </section>      
+        </form>
+    </main>
+</body>
+</html>
