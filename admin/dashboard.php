aa3b84f6095560018e218a1410d58081d11411d6
ben devos <bendevos07@gmail.com>
2024-11-19 22:57:38 +0100

    feat: add admin dashboard

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -5,15 +5,19 @@
         header("Location: ../index.php");
         exit();
     }
-
 ?><!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Admin Dashboard | Cardforge</title>
+    <?php include_once __DIR__."/../includes/adminstylesheets.inc.php";?>
 </head>
 <body>
-    welcome to the admin dashboard
+    <?php include_once __DIR__."/../includes/adminheader.inc.php";?>
+    <main>
+        <h1>Admin Dashboard</h1>
+        <a href="./add-product.php" class="btn">Add New Product</a>
+    </main>
 </body>
 </html>
---
+++ includes/adminheader.inc.php
@@ -0,0 +1,8 @@
+<header>
+    <nav>
+        <a href="index.php" class="logo"></a>
+        <h2 class="app-name">cardforge</h2>
+        <label for="hb-account" class="header-avatar" style="background-image: url('./../assets/img/user-avatar/default.jpg');"></label>
+        <input type="checkbox" id="hb-account" class="hidden">
+    </nav>
+</header>
