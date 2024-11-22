43eae4fc0f204157785c5786a09fc034d87dd797
ben devos <bendevos07@gmail.com>
2024-11-22 15:54:16 +0100

    feat: Category class

---
+++ src/Category.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace Codinari\Cardforge;
+
+use Codinari\Cardforge\Db;
+use Codinari\Cardforge\Franchise;
+
+class Category{
+    private $name;
+    private $alias;
+    private $franchise;
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setName($name)
+    {
+        if (empty($name)) {
+            throw new \Exception("Name can't be empty");
+        } else {
+            $this->name = $name;
+            return $this;
+        }
+    }
+
+    public function getAlias()
+    {
+        return $this->alias;
+    }
+
+    public function setAlias()
+    {
+        //generate a unique alias to be used in url
+        $alias = strtolower(str_replace(" ", "-", $this->name));
+        //also remove specialchars from alias
+        $alias = preg_replace('/[^A-Za-z0-9\-]/', '', $alias);
+        $this->alias = $alias;
+        return $this;
+    }
+
+    public function getFranchise()
+    {
+        return $this->franchise;
+    }
+
+    public function setFranchise($franchise)
+    {
+        if (empty($franchise)) {
+            throw new \Exception("Franchise can't be empty");
+        } else {
+            $franchise = Franchise::getByAlias($franchise);
+
+            $this->franchise = $franchise;
+            return $this;
+        }
+    }
+
+    public function save(){
+        $conn = Db::getConnection();
+
+        $query = 'INSERT INTO categories (name, alias, franchise_id) VALUES (
+            :name, 
+            :alias, 
+            (SELECT franchises.id FROM franchises WHERE franchises.alias = :franchise)
+        )';
+
+        $stmt = $conn->prepare($query);
+        $stmt->bindValue(':name', $this->name);
+        $stmt->bindValue(':alias', $this->alias);
+        $stmt->bindValue(':franchise', $this->franchise);
+
+        $result = $stmt->execute();
+
+        if($result){
+            return true;
+        }else{
+            throw new \Exception("Failed to save category");
+        }
+    }
+
+    public static function getAll(){
+        $conn = Db::getConnection();
+
+        $query = 'SELECT * FROM categories';
+
+        $stmt = $conn->prepare($query);
+        $stmt->execute();
+
+        $categories = $stmt->fetchAll();
+
+        return $categories;
+    }
+
+    public static function getAllByFranchise($franchise){
+        $conn = Db::getConnection();
+
+        $query = 'SELECT * FROM categories WHERE franchise_id = (SELECT franchises.id FROM franchises WHERE franchises.alias = :franchise)';
+
+        $stmt = $conn->prepare($query);
+        $stmt->bindValue(':franchise', $franchise);
+        $stmt->execute();
+
+        $categories = $stmt->fetchAll();
+
+        return $categories;
+    }
+}
