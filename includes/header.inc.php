53a8f663f00b25eab3582c2f179470545b4f688e
ben devos <bendevos07@gmail.com>
2024-11-27 12:05:34 +0100

    refactor: navigating to home by click on appname

--- includes/header.inc.php
+++ includes/header.inc.php
@@ -1,7 +1,7 @@
 <header>
     <nav>
         <a href="index.php" class="logo"></a>
-        <h2 class="app-name">cardforge</h2>
+        <a href="index.php"><h2 class="app-name">cardforge</h2></a>
         <?php if(empty($user)):?>
             <a href="#" class="header-avatar" style="background-image: url('https://res.cloudinary.com/codinari/image/upload/v1732362678/rxtz6j9unmhj7wliqdya.jpg');"></a>
         <?php else:?>
