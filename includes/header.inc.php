459d6de135892a5463e8676b32d7967948bd22f8
ben devos <bendevos07@gmail.com>
2024-12-08 18:56:16 +0100

    style: make header responsive

--- includes/header.inc.php
+++ includes/header.inc.php
@@ -1,11 +1,11 @@
-<header>
-    <nav>
+<header id="header">
+    <nav class="mobile-nav">
         <a href="index.php" class="logo"></a>
         <a href="index.php"><h2 class="app-name">cardforge</h2></a>
         <?php if(empty($user)):?>
             <a href="#" class="header-avatar" style="background-image: url('https://res.cloudinary.com/codinari/image/upload/v1732362678/rxtz6j9unmhj7wliqdya.jpg');"></a>
         <?php else:?>
-            <a href="login.php" class="header-avatar" style="background-image: url('<?=$user->getAvatar()?>');"></a>
+            <a href="" class="header-avatar header-avatar--mobile" style="background-image: url('<?=$user->getAvatar()?>');"></a>
         <?php endif;?>
     </nav>
     <nav class="mobile-search">
@@ -16,6 +16,24 @@
             <input type="text" name="search-request" placeholder="I'm looking for...">
             <input type="submit" name="search-btn" value="" class="search-icon" class="search-btn">
         </form>
+    </nav>
+    <nav class="desktop-nav">
+        <div class="logo-wrap">
+            <a href="index.php" class="logo"></a>
+            <a href="index.php"><h2 class="app-name">cardforge</h2></a>
+        </div>
+        <form class="search-bar" method="post" action="">
+            <input type="text" name="search-request" placeholder="I'm looking for...">
+            <input type="submit" name="search-btn" value="" class="search-icon" class="search-btn">
+        </form>
+        <a href="./cart.php" class="header-cart cart-icon cart-icon--white">
+            <span id="cart-count"><?= count($_SESSION['cart'] ?? [])?></span>
+        </a>
+        <?php if(empty($user)):?>
+            <a href="#" class="header-avatar header-avatar--desktop" style="background-image: url('https://res.cloudinary.com/codinari/image/upload/v1732362678/rxtz6j9unmhj7wliqdya.jpg');"></a>
+        <?php else:?>
+            <a href="" class="header-avatar header-avatar--desktop" style="background-image: url('<?=$user->getAvatar()?>');"></a>
+        <?php endif;?>
     </nav>
     <nav class="account-nav">
         <?php if(empty($user)):?>
--- js/accountNav.js
+++ js/accountNav.js
@@ -1,7 +1,11 @@
-const headerAvatar = document.querySelector('.header-avatar');
 const accountNav = document.querySelector('.account-nav');
 
-headerAvatar.addEventListener('click', function(e){
+document.querySelector('.header-avatar--mobile').addEventListener('click', function(e){
+    e.preventDefault();
+    accountNav.classList.toggle('active');
+});
+
+document.querySelector('.header-avatar--desktop').addEventListener('click', function(e){
     e.preventDefault();
     accountNav.classList.toggle('active');
 });
