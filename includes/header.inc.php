649b0d8fa6a605aaf0935896bc790ba4b847f925
ben devos <bendevos07@gmail.com>
2024-12-08 21:04:37 +0100

    fix: header nav not working when deployed

--- includes/header.inc.php
+++ includes/header.inc.php
@@ -3,7 +3,7 @@
         <a href="index.php" class="logo"></a>
         <a href="index.php"><h2 class="app-name">cardforge</h2></a>
         <?php if(empty($user)):?>
-            <a href="#" class="header-avatar" style="background-image: url('https://res.cloudinary.com/codinari/image/upload/v1732362678/rxtz6j9unmhj7wliqdya.jpg');"></a>
+            <a href="#" class="header-avatar header-avatar--mobile" style="background-image: url('https://res.cloudinary.com/codinari/image/upload/v1732362678/rxtz6j9unmhj7wliqdya.jpg');"></a>
         <?php else:?>
             <a href="" class="header-avatar header-avatar--mobile" style="background-image: url('<?=$user->getAvatar()?>');"></a>
         <?php endif;?>
